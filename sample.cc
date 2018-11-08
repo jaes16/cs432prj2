f0e3a112ec3e411e08ff800d0ef1aafb2c9fc09d
Jae Mahn Surh <20js16@lohani.cs.williams.edu>
2018-11-07 20:26:23 -0500

    hi

---
+++ sample.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "vm_app.h"
+
+using namespace std;
+
+int main()
+{
+  char *p;
+  cout << "hi" << endl;
+  p = (char *) vm_extend();
+  cout << "got here" << endl;
+  p[0] = 'h';
+  p[1] = 'e';
+  p[2] = 'l';
+  p[3] = 'l';
+  p[4] = 'o';
+}
